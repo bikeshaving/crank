50d25c97ba617075ccd214cc9d1881e117ee2709
Brian Kim <briankimpossible@gmail.com>
2019-12-21 04:12:04 -0500

    apparently jest.config.js is prettiered

--- jest.config.js
+++ jest.config.js
@@ -146,10 +146,7 @@ module.exports = {
 	// ],
 
 	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-	testPathIgnorePatterns: [
-	  "/node_modules/",
-    "/_[^_].*",
-	],
+	testPathIgnorePatterns: ["/node_modules/", "/_[^_].*"],
 
 	// The regexp pattern or array of patterns that Jest uses to detect test files
 	// testRegex: [],
