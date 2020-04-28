5371d286c0bdee88e670b6df0c506cde74c933c3
Brian Kim <briankimpossible@gmail.com>
2020-04-28 13:54:57 -0400

    prevent cascades from blowing the stack/iterating over a generator while it is still running

--- src/__tests__/index.tsx
+++ src/__tests__/index.tsx
@@ -1992,3 +1992,98 @@ describe("async races", () => {
 		expect(document.body.innerHTML).toEqual("<div><span>Slow</span></div>");
 	});
 });
+
+describe("parent-child refresh cascades", () => {
+	test("sync function parent and sync function child", () => {
+		return new Promise((done) => {
+			function Child(this: Context) {
+				this.dispatchEvent(new Event("test", {bubbles: true}));
+				return <span>child</span>;
+			}
+
+			function Parent(this: Context) {
+				this.addEventListener("test", () => {
+					try {
+						this.refresh();
+						done();
+					} catch (err) {
+						done(err);
+					}
+				});
+
+				return (
+					<div>
+						<Child />
+					</div>
+				);
+			}
+
+			renderer.render(<Parent />, document.body);
+			expect(document.body.innerHTML).toEqual("<div><span>child</span></div>");
+		});
+	});
+
+	test("sync generator parent and sync function child", () => {
+		return new Promise((done) => {
+			function Child(this: Context) {
+				this.dispatchEvent(new Event("test", {bubbles: true}));
+				return <span>child</span>;
+			}
+
+			function* Parent(this: Context) {
+				this.addEventListener("test", () => {
+					try {
+						this.refresh();
+						done();
+					} catch (err) {
+						done(err);
+					}
+				});
+
+				while (true) {
+					yield (
+						<div>
+							<Child />
+						</div>
+					);
+				}
+			}
+
+			renderer.render(<Parent />, document.body);
+			expect(document.body.innerHTML).toEqual("<div><span>child</span></div>");
+		});
+	});
+
+	test("sync generator parent and sync generator child", () => {
+		return new Promise((done) => {
+			function* Child(this: Context) {
+				while (true) {
+					this.dispatchEvent(new Event("test", {bubbles: true}));
+					yield <span>child</span>;
+				}
+			}
+
+			function* Parent(this: Context) {
+				this.addEventListener("test", () => {
+					try {
+						this.refresh();
+						done();
+					} catch (err) {
+						done(err);
+					}
+				});
+
+				while (true) {
+					yield (
+						<div>
+							<Child />
+						</div>
+					);
+				}
+			}
+
+			renderer.render(<Parent />, document.body);
+			expect(document.body.innerHTML).toEqual("<div><span>child</span></div>");
+		});
+	});
+});
