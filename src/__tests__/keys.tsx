59616be367c026ce66c3c126cf17b795b2887f3e
Brian Kim <briankimpossible@gmail.com>
2020-05-15 07:49:31 -0400

    add some more key tests

--- src/__tests__/keys.tsx
+++ src/__tests__/keys.tsx
@@ -2,6 +2,7 @@
 import {createElement} from "../index";
 import {renderer} from "../dom";
 
+// TODO: write generative tests for this stuff
 describe("keys", () => {
 	afterEach(() => {
 		document.body.innerHTML = "";
@@ -249,6 +250,31 @@ describe("keys", () => {
 		expect(document.body.firstChild!.firstChild).not.toBe(span);
 	});
 
+	test("keyed children added before removed unkeyed child", () => {
+		renderer.render(
+			<div>
+				<div crank-key="1">1</div>
+				<span>2</span>
+			</div>,
+			document.body,
+		);
+		expect(document.body.innerHTML).toEqual(
+			"<div><div>1</div><span>2</span></div>",
+		);
+		renderer.render(
+			<div>
+				<span crank-key="1">1</span>
+				<span crank-key="2">2</span>
+				<span crank-key="3">3</span>
+				<span crank-key="4">4</span>
+			</div>,
+			document.body,
+		);
+		expect(document.body.innerHTML).toEqual(
+			"<div><span>1</span><span>2</span><span>3</span><span>4</span></div>",
+		);
+	});
+
 	test("same key, different tag", () => {
 		renderer.render(
 			<div>
@@ -326,5 +352,111 @@ describe("keys", () => {
 		expect(span2).toEqual(document.body.firstChild!.childNodes[1]);
 		expect(span3).toEqual(document.body.firstChild!.childNodes[2]);
 		expect(span4).toEqual(document.body.firstChild!.childNodes[3]);
+	});
+
+	test("changing tag of keyed child", () => {
+		renderer.render(
+			<div>
+				<span>0</span>
+				<span crank-key="1">1</span>
+			</div>,
+			document.body,
+		);
+		expect(document.body.innerHTML).toEqual(
+			"<div><span>0</span><span>1</span></div>",
+		);
+		renderer.render(
+			<div>
+				<div crank-key="1">1</div>
+				<span>0</span>
+			</div>,
+			document.body,
+		);
+		expect(document.body.innerHTML).toEqual(
+			"<div><div>1</div><span>0</span></div>",
+		);
+		renderer.render(
+			<div>
+				<span>0</span>
+				<span crank-key="1">1</span>
+			</div>,
+			document.body,
+		);
+		expect(document.body.innerHTML).toEqual(
+			"<div><span>0</span><span>1</span></div>",
+		);
+		renderer.render(
+			<div>
+				<div crank-key="1">1</div>
+				<span>0</span>
+			</div>,
+			document.body,
+		);
+		expect(document.body.innerHTML).toEqual(
+			"<div><div>1</div><span>0</span></div>",
+		);
+	});
+
+	test("swapping", () => {
+		renderer.render(
+			<div>
+				<span crank-key="1">1</span>
+				<span crank-key="2">2</span>
+				<span crank-key="3">3</span>
+				<span crank-key="4">4</span>
+				<span crank-key="5">5</span>
+				<span crank-key="6">6</span>
+			</div>,
+			document.body,
+		);
+		expect(document.body.innerHTML).toEqual(
+			"<div><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span></div>",
+		);
+		const span1 = document.body.firstChild!.childNodes[0];
+		const span2 = document.body.firstChild!.childNodes[1];
+		const span3 = document.body.firstChild!.childNodes[2];
+		const span4 = document.body.firstChild!.childNodes[3];
+		const span5 = document.body.firstChild!.childNodes[4];
+		const span6 = document.body.firstChild!.childNodes[5];
+		renderer.render(
+			<div>
+				<span crank-key="1">1</span>
+				<span crank-key="5">5</span>
+				<span crank-key="3">3</span>
+				<span crank-key="4">4</span>
+				<span crank-key="2">2</span>
+				<span crank-key="6">6</span>
+			</div>,
+			document.body,
+		);
+		expect(document.body.innerHTML).toEqual(
+			"<div><span>1</span><span>5</span><span>3</span><span>4</span><span>2</span><span>6</span></div>",
+		);
+		expect(span1).toEqual(document.body.firstChild!.childNodes[0]);
+		expect(span2).toEqual(document.body.firstChild!.childNodes[4]);
+		expect(span3).toEqual(document.body.firstChild!.childNodes[2]);
+		expect(span4).toEqual(document.body.firstChild!.childNodes[3]);
+		expect(span5).toEqual(document.body.firstChild!.childNodes[1]);
+		expect(span6).toEqual(document.body.firstChild!.childNodes[5]);
+		renderer.render(
+			<div>
+				<span crank-key="1">1</span>
+				<span crank-key="2">2</span>
+				<span crank-key="3">3</span>
+				<span crank-key="4">4</span>
+				<span crank-key="5">5</span>
+				<span crank-key="6">6</span>
+			</div>,
+			document.body,
+		);
+		expect(document.body.innerHTML).toEqual(
+			"<div><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span></div>",
+		);
+		expect(span1).toEqual(document.body.firstChild!.childNodes[0]);
+		expect(span2).toEqual(document.body.firstChild!.childNodes[1]);
+		expect(span3).toEqual(document.body.firstChild!.childNodes[2]);
+		expect(span4).toEqual(document.body.firstChild!.childNodes[3]);
+		expect(span5).toEqual(document.body.firstChild!.childNodes[4]);
+		expect(span6).toEqual(document.body.firstChild!.childNodes[5]);
 	});
 });
