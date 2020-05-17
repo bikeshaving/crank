6d247694a1e0087ac4866e66d06273cd6adbf1f4
Brian Kim <briankimpossible@gmail.com>
2020-05-17 16:39:55 -0400

    use cjs for jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-export default {
+module.exports = {
 	// All imported modules in your tests should be mocked automatically
 	// automock: false,
 
