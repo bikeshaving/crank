5d9a28d52efbf6d3b3349d0345b3b66741873cb4
Brian Kim <briankimpossible@gmail.com>
2020-05-16 20:04:33 -0400

    build stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,55 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
-export default {
-	input: ["src/index.ts", "src/dom.ts", "src/html.ts"],
-	output: [
-		{
-			format: "cjs",
-			dir: "cjs",
-			sourcemap: true,
+import * as fs from "fs";
+import * as path from "path";
+
+function packageCJS() {
+	return {
+		name: "packageCJS",
+		writeBundle(options) {
+			const dir = options.dir;
+			fs.writeFileSync(
+				path.join(__dirname, dir, "package.json"),
+				JSON.stringify({type: "commonjs"}),
+			);
 		},
-		{
+	};
+}
+
+export default [
+	{
+		input: ["src/index.ts", "src/dom.ts", "src/html.ts"],
+		output: {
 			format: "esm",
 			dir: "esm",
 			sourcemap: true,
 		},
-	],
-	plugins: [typescript(), resolve()],
-};
+		plugins: [typescript(), resolve()],
+	},
+	{
+		input: ["src/index.ts", "src/dom.ts", "src/html.ts"],
+		output: {
+			format: "cjs",
+			dir: "cjs",
+			sourcemap: true,
+		},
+		plugins: [
+			typescript({tsconfigOverride: {compilerOptions: {target: "ES6"}}}),
+			resolve(),
+			packageCJS(),
+		],
+	},
+	{
+		input: "umd.ts",
+		output: {
+			format: "umd",
+			dir: "umd",
+			sourcemap: true,
+			name: "Crank",
+		},
+		plugins: [
+			typescript({tsconfigOverride: {compilerOptions: {target: "ES6"}}}),
+			resolve(),
+		],
+	},
+];
---
+++ umd.ts
@@ -0,0 +1,4 @@
+// input file for the UMD build
+export * from "./src/index";
+export * as dom from "./src/dom";
+export * as html from "./src/html";
