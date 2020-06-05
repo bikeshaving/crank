5dffc402b352ba8b69d7b2c879a7be2b48d7316e
Brian Kim <briankimpossible@gmail.com>
2020-06-05 15:10:16 -0400

    move some element props to contexts

--- src/flags.ts
+++ src/flags.ts
@@ -3,14 +3,14 @@ export const Dirty = 1 << 0;
 export const Moved = 1 << 1;
 export const Mounted = 1 << 2;
 export const Updating = 1 << 3;
-export const Stepping = 1 << 4;
-export const Iterating = 1 << 5;
-export const Handling = 1 << 6;
-export const Available = 1 << 7;
-export const Removing = 1 << 8;
-export const Finished = 1 << 9;
-export const Unmounted = 1 << 10;
+export const Handling = 1 << 4;
+export const Removing = 1 << 5;
+export const Finished = 1 << 6;
+export const Unmounted = 1 << 7;
+// Context-only Flags
+export const Iterating = 1 << 8;
+export const Stepping = 1 << 9;
+export const Available = 1 << 10;
 export const SyncGen = 1 << 11;
 export const AsyncGen = 1 << 12;
-
 export const Initial = Dirty | Moved;
