98ff753e0cd85e7ec949bee5c5704cdfbd84b37a
Brian Kim <briankimpossible@gmail.com>
2020-07-23 01:28:54 -0400

    tweaks to the example

--- examples/transitions/src/index.jsx
+++ examples/transitions/src/index.jsx
@@ -4,7 +4,6 @@ import {renderer} from "@bikeshaving/crank/dom";
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 function shuffle(arr) {
-	arr = arr.slice();
 	for (let i = arr.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
 		[arr[i], arr[j]] = [arr[j], arr[i]];
@@ -14,60 +13,59 @@ function shuffle(arr) {
 }
 
 function randomLetters() {
-	return shuffle(alphabet)
+	return shuffle(Array.from(alphabet))
 		.slice(0, Math.floor(Math.random() * 26))
 		.sort();
 }
 
 async function* Letter({letter, index}) {
-	let em = index * 1.3;
 	let node;
 	try {
 		const style = {
 			position: "absolute",
-			transition: "transform .7s",
-			transform: `translate(${em}em, -30px)`,
-			color: "green",
+			top: "20px",
+			transition: "transform 750ms, opacity 750ms",
 		};
+
 		node = yield <span style={style}>{letter}</span>;
+		node.style.color = "green";
+		node.style.transform = `translate(${index * 1.2}em, -20px)`;
+		node.style.opacity = 0;
 		requestAnimationFrame(() => {
-			node.style.transform = `translate(${em}em, 0)`;
+			node.style.transform = `translate(${index * 1.2}em, 0)`;
+			node.style.opacity = 1;
 		});
 
 		for await ({letter, index} of this) {
-			em = index * 1.3;
-			yield <span>{letter}</span>;
+			yield <span style={style}>{letter}</span>;
+			node.style.color = "black";
 			requestAnimationFrame(() => {
-				node.style.color = "black";
-				node.style.transform = `translate(${em}em, 0)`;
+				node.style.transform = `translate(${index * 1.2}em, 0)`;
 			});
 		}
 	} finally {
-		if (node !== undefined) {
-			requestAnimationFrame(() => {
-				node.style.color = "red";
-				node.style.transform = `translate(${em}em, 30px)`;
-			});
-
-			await new Promise((resolve) => setTimeout(resolve, 700));
-		}
+		node.style.color = "red";
+		requestAnimationFrame(() => {
+			node.style.transform = `translate(${index * 1.2}em, 20px)`;
+			node.style.opacity = 0;
+		});
+		await new Promise((resolve) => setTimeout(resolve, 750));
 	}
 }
 
 function* Letters() {
 	const interval = setInterval(() => this.refresh(), 1500);
-
 	try {
 		while (true) {
 			const letters = randomLetters().map((l, i) => (
 				<Letter crank-key={l} letter={l} index={i} />
 			));
 
-			yield <div style="margin: 50px 20px;">{letters}</div>;
+			yield <div style="height: 40px">{letters}</div>;
 		}
 	} finally {
 		clearInterval(interval);
 	}
 }
 
-renderer.render(<Letters letter="a" />, document.body);
+renderer.render(<Letters />, document.body);
