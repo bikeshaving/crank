5ca9af1282b62e5ff77d65d1cea26fa258556ee0
Brian Kim <briankimpossible@gmail.com>
2021-10-10 23:13:55 -0400

    ignore dist in tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -81,7 +81,7 @@ module.exports = {
 	// moduleNameMapper: {},
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-	// modulePathIgnorePatterns: [],
+	modulePathIgnorePatterns: ["./dist"],
 
 	// Activates notifications for test results
 	// notify: false,
