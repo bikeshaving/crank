95f70c4560f70a403d14b59d633e64cc9b67f4e8
Brian Kim <brainkim@fuckyou.com>
2022-06-07 15:32:02 -0700

    move thoughts to a separate file

--- src/template.ts
+++ src/template.ts
@@ -1,4 +1,4 @@
-import {createElement, Element} from "./crank.js";
+import {c, Element} from "./crank.js";
 import type {Tag} from "./crank.js";
 
 // TODO: Handle illegal escape sequences.
@@ -25,129 +25,7 @@ interface ParseElementResult {
 	children: Array<ParseElementResult | string>;
 }
 
-/* Whitespace
-The JSX whitespace rules are complicated and controversial.
-
-In the early days of React, when the JSX syntax extension was still being
-implemented in the popular transpilers of the time, contributors debated how
-whitespace should work. Of course, they shouldn’t work like HTML’s because
-those rules are kinda insane. Insofar as JSX is meant for programmers, who like
-to fiddle with whitespace, it should follow the same semantics as JavaScript
-itself. One contributor writes:
-
-	Indenting/beautifying code should never affect the outcome, especially as it
-	means all indentation has to carry over into the final result and cannot be
-	minified away (yuck!). While there are a handful of cases where you might
-	want newlines to affect the outcome, those are far better handled explicitly
-	by {' '} or {'\n'}.
-	https://github.com/facebook/jsx/issues/19#issuecomment-57079949
-
-We can rephrase the desire for “beauty” here as a desire for some whitespace in
-the JSX grammar to be semantic-insensitive, for the purposes of programmers and
-tools to toy with. Common use-cases for developer tooling like formatters and
-minifiers include highlighting the “structure” of elements via indentation, or
-trimming accidental whitespace. These use-cases can be rephrased as the
-concrete rule: whitespace at the starts and ends of lines, or around the
-children of an element, can be added or removed without affecting the semantics
-of the code, ”semantics” here meaning simply the transpilation of JSX to
-createElement() calls.
-
-The tricky part for JSX is the use-case mentioned previously, where developers
-want “some” whitespace at the ends of lines to be significant, as between two
-inline elements divided across lines. As the author indicates, the solution is
-to use JSX’s interpolation syntax `{" "}` to do so.
-
-	return (
-		<div>
-			<span>Hello</span>{" "}
-			<span>World</span>
-		</div>
-	);
-
-Unfortunately, this solution is a pitfall for developers new to JSX, who
-regularly assume that the whitespace rules for JSX are the same or similar to
-that of HTML, as evidenced in the issue trackers for downstream transpilers.
-Ironically, this solution is in and of itself “ugly,” or at least requires more
-characters to type out, and a bigger lift for authors of developer tools.
-
-To ease the tension, either developer tools need a semantic understanding of
-inline/block elements to know how to preserve whitespace between elements, or
-the whitespace rules of JSX need to give a little. The former solution is
-futile, insofar as the semantics of inline and block elements can also depend
-on styling. Meanwhile, there has been no progress made in regards to the latter
-solution. Hence we are at the impasse we are at today.
-
-The root cause of all this worry is that the JSX grammar does not have a way to
-indicate that whitespace at the ends of lines is significant. Thankfully,
-JavaScript template strings allow for Unix-style escapes of newlines, so we
-don’t have to deal with any of these problems!
-
-	yield x`
-		<div>
-			<span>Hello</span> \
-			<span>World</span>
-		</div>
-	`;
-
-The only catch is that we have to use the raw representation of strings,
-because the “cooked” representation does not indicate that the newline has been
-escaped; it removes the newline from the template tag entirely. For instance,
-as we parse the template in the previous example, we want to treat the tabs on
-the next line before `<span>World</span>` as insignificant, but we would have
-no way of knowing that the tabs were at the start of a new line, rather than
-between two elements.
-*/
-
-/* Some rambling thoughts on closing tags
-JSX uses upper-case capitalization to determine whether a tag is an identifier
-from the scope. The disadvantage for template tags is that if we interpolate
-tags, this is slightly more typing (x`<${Component} />`). This unwieldy-ness is
-exacerbated by closing tags, which must match opening tags to form a
-well-formed tree.
-
-	yield x`
-		<${Component}>hello</${Component}>
-	`;
-
-Having to type out the `${}` twice is twice the syntactic noise. Honestly, as I
-look at the above example, it doesn’t seem too bad, but insofar as JSX is not
-HTML, we can do what developit/htm does and have a catch-all closing tag like
-`<//>`.
-
-	yield x`
-		<${Component}>hello<//>
-	`;
-
-I think this would be the most explicit, least surprising design, right? Trying
-to do fancy things where we allow closing elements to be un-interpolated, and
-then parsed and compared against function names, for instance, would get in the
-way of minifiers, which regularly mess with function names. Using the
-double-slash to indicate we want to opt out of the well-formed XML constraint
-is nice and explicit. We could also treat the double slash as a comment-like
-construct, where you could put anything after the slashes.
-
-	yield x`
-		<${Component}>hello<//Component>
-	`;
- */
-
 // TODO: Add HTML style comments to the grammar `<!-- comment -->`
-
-/* Grammar
-$CHILDREN: ($CHILD_TEXT | $ELEMENT | ${unknown})*
-$CHILD_TEXT: /[^<]+/
-$ELEMENT:
-  $SELF_CLOSING_ELEMENT |
-  $OPENING_ELEMENT $CHILDREN $CLOSING_ELEMENT
-$SELF_CLOSING_ELEMENT: "<" ($IDENTIFIER | ${Tag}) $PROPS "/" ">"
-$OPENING_ELEMENT: "<" ($IDENTIFIER | ${Tag})? $PROPS ">"
-$CLOSING_ELEMENT: "<" "/" "/"? ($IDENTIFIER)? ">"
-$PROPS: ($PROP | $SPREAD_PROP)*
-$SPREAD_PROP: "..." ${Record<string, unknown>}
-$PROP: $IDENTIFIER ("=" $PROP_VALUE)?
-$IDENTIFIER: /[^\s>]+/
-$PROP_VALUE: /"[^"]*"/ | /'[^']*'/ | ${unknown}
-*/
 
 // I wish we could use regexs but we have to deal with expressions...
 // We can probably deal with expressions with the string terminating regexp
@@ -286,7 +164,7 @@ function createElementsFromParse(
 	// TODO: actually do this bro
 	const result: Array<Element> = [];
 	for (let i = 0; i < parsed.length; i++) {
-		result.push(createElement("p"));
+		result.push(c("p"));
 	}
 
 	if (result.length === 1) {
@@ -294,5 +172,5 @@ function createElementsFromParse(
 	}
 
 	// return a fragment
-	return createElement("", null, result);
+	return c("", null, result);
 }
---
+++ website/documents/template-design.md
@@ -0,0 +1,153 @@
+# Designing a template language in the spirit of JSX.
+
+## Grammar
+
+```
+$CHILDREN: ($CHILD_TEXT | $ELEMENT | ${unknown})*
+$CHILD_TEXT: /[^<]+/
+$ELEMENT:
+  $SELF_CLOSING_ELEMENT |
+  $OPENING_ELEMENT $CHILDREN $CLOSING_ELEMENT
+$SELF_CLOSING_ELEMENT: "<" ($IDENTIFIER | ${Tag}) $PROPS "/" ">"
+$OPENING_ELEMENT: "<" ($IDENTIFIER | ${Tag})? $PROPS ">"
+$CLOSING_ELEMENT: "<" "/" "/"? ($IDENTIFIER)? ">"
+$PROPS: ($PROP | $SPREAD_PROP)*
+$SPREAD_PROP: "..." ${Record<string, unknown>}
+$PROP: $IDENTIFIER ("=" $PROP_VALUE)?
+$IDENTIFIER: /[^\s>]+/
+$PROP_VALUE:
+  (/"/ (/[^"]/ | ${unknown})* /"/) |
+  (/'/ (/[^']/ | ${unknown})* /'/)
+```
+
+## Whitespace
+
+The JSX whitespace rules are complicated and controversial.
+
+In the early days of React, when the JSX syntax extension was still being
+implemented in the popular transpilers of the time, contributors debated how
+whitespace should work. Of course, it shouldn’t work like HTML’s whitespace,
+because those rules are flexible, some might even say insane, a result of
+HTML’s original goal as a markup language. Insofar as JSX is meant for
+programmers, who spend their waking hours editing text files, for fun or for
+profit, and like to fiddle with the whitespace in these files, they should
+follow the same semantics as JavaScript itself. To this extent, one contributor
+wrote:
+
+> Indenting/beautifying code should never affect the outcome, especially as it
+> means all indentation has to carry over into the final result and cannot be
+> minified away (yuck!). While there are a handful of cases where you might
+> want newlines to affect the outcome, those are far better handled explicitly
+> by {' '} or {'\n'}.
+
+https://github.com/facebook/jsx/issues/19#issuecomment-57079949
+
+We can rephrase this as a desire for some whitespace in the JSX grammar to be
+semantic-insensitive, for the purposes of programmers and tools to toy with.
+Common use-cases include highlighting the “structure” of elements via
+indentation, and trimming accidental whitespace at the ends of lines. These
+use-cases can be rephrased as the concrete rule: whitespace at the starts and
+ends of lines, or around the children of an element, can be added or removed
+without affecting the semantics of the code, ”semantics” here meaning simply,
+the transpilation of JSX to `createElement()` calls.
+
+The tricky part for JSX is the use-case mentioned previously, where developers
+want “some” whitespace at the ends of lines to be significant, as between
+“inline” elements separated by a newline. As the author indicates, the solution
+is to use JSX’s interpolation syntax `{" "}` to do so.
+
+```
+return (
+  <div>
+    <span>Hello</span>{" "}
+    <span>World</span>
+  </div>
+);
+```
+
+This a pitfall for developers new to JSX, who regularly assume that the
+whitespace rules are the same or similar to that of HTML, as evidenced in issue
+trackers for JSX transpilers. Additionally, this solution is in and of itself
+“ugly,” or at the very least, it requires more characters to type out, and is a
+bigger lift for authors of developer tools to manage.
+
+To ease the tension, either developer tools need a semantic understanding of
+inline/block tags to preserve whitespace across lines, or the whitespace rules
+of JSX need to give a little. The former solution is futile, insofar as the
+semantics of inline and block can depend on styling in the browser. Meanwhile,
+there’s been no progress made with regard to JSX.
+
+The root cause of all this worry is that the JSX grammar does not have a way to
+indicate that whitespace at the ends of lines is significant. Thankfully,
+JavaScript template strings allow for Unix-style escapes of newlines, so we
+don’t have to deal with any of these problems!
+
+```
+yield x`
+  <div>
+    <span>Hello</span> \
+    <span>World</span>
+  </div>
+`;
+```
+
+The only catch is that we have to use the raw representation of strings,
+because the “cooked” representation does not indicate that the newline has been
+escaped; it removes the newline from the text input. For instance, as we parse
+the template in the previous example, we want to treat the whitespace on the
+line before `<span>World</span>` as insignificant, but we would have no way of
+knowing that these tabs were at the start of a new line.
+
+## Some rambling thoughts on closing tags
+
+JSX uses upper-case capitalization to determine whether a tag is an identifier
+from the scope. The disadvantage for templates is that if we interpolate them,
+this is slightly more typing (x`<${Component} />`). This unwieldy-ness is
+exacerbated by closing tags, which must match opening tags to form a
+well-formed tree.
+
+```
+yield x`
+  <${Component}>hello</${Component}>
+`;
+```
+
+Having to type out the `${}` twice is a bit of extra syntactic noise. Honestly,
+as I look at the above example, it doesn’t seem too bad, but insofar as JSX is
+not HTML, we can do what developit/htm does and have a catch-all closing tag
+syntax like `<//>`.
+
+```
+yield x`
+  <${Component}>hello<//>
+`;
+```
+
+I think this would be the most explicit, least surprising design, right? Trying
+to do fancy things where we allow closing elements to be un-interpolated, and
+then compared against function names, for instance, would get in the way of
+minifiers, which regularly mess with function names. Using the double-slash to
+indicate we want to opt out of the well-formed XML constraint is nice and
+explicit. We could also treat the double slash as a comment-like construct,
+where you could put anything after the slashes. This is a fun thing that future
+linters could fight about.
+
+```
+// Maybe a linter could be used to determine if the rules
+yield x`
+  <${Component}>hello<//Component>
+`;
+```
+
+Little things:
+
+Should we allow for interpolations in prop strings?
+
+```
+const cls = 1;
+yield x`
+  <div class="cls-${cls}">Hello</div>
+`;
+```
+
+Why not!? It’s just nice and convenient, right?
