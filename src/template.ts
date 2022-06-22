e11ea8ade5f33e0fa8c9905caa0856244e5e868f
Brian Kim <brainkim@fuckyou.com>
2022-06-22 11:00:35 -0700

    get rid of modes

--- src/template.ts
+++ src/template.ts
@@ -34,14 +34,6 @@ interface ParseValueResult {
 
 type ParseResult = ParseElementResult | ParseValueResult;
 
-/* Modes */
-// TODO: In the shower I had a fun little clever idea where we use the regular
-// expressions directly to signify modes
-const CHILDREN_MODE = 0;
-const PROPS_MODE = 1;
-const CLOSING_TAG_MODE = 2;
-const CLOSING_COMMENT_MODE = 3;
-
 /*
  * Matches the first significant character in children mode.
  * Group 1: newline
@@ -50,7 +42,7 @@ const CLOSING_COMMENT_MODE = 3;
  * Group 4: closing slash
  * Group 5: tag name
  */
-const CHILDREN_RE =
+const CHILDREN_MATCHER =
 	/((?:\r|\n|\r\n)\s*)|(<!--[\S\s]*?(?:-->|$))|(<\s*(\/{0,2})\s*(?:([-\w]*)\s*|$))/g;
 
 // TODO: Think about prop name character class
@@ -61,11 +53,14 @@ const CHILDREN_RE =
  * Group 3: prop name
  * Group 4: prop value string
  */
-const PROPS_RE =
+const PROPS_MATCHER =
 	/\s*(\/?\s*>)|(\.\.\.\s*$)|(?:([-\w]+)\s*(?:=\s*(?:("[^"]*"|'[^']*')|$))?)/g;
 
 /* Matches closing tag */
-const CLOSING_TAG_RE = /\s*>/g;
+const CLOSING_TAG_MATCHER = /\s*>/g;
+
+/* Matches a closing comment. */
+const CLOSING_COMMENT_MATCHER = "-->";
 
 function parseChildren(
 	spans: ArrayLike<string>,
@@ -78,159 +73,162 @@ function parseChildren(
 		children: [],
 	};
 	const stack: Array<ParseElementResult> = [];
-	let mode = CHILDREN_MODE;
+	let matcher = CHILDREN_MATCHER as RegExp | string;
 	let lineStart = true;
 	for (let s = 0; s < spans.length; s++) {
 		const span = spans[s];
-		// a variable which we use to
-		// expressions[spans.length - 1] will never be defined because template
-		// tags are always called with one more span than expression.
+		// expressions[spans.length - 1] will never be defined because
+		// template tags are always called with one more span than expression.
 		let expressing = s < spans.length - 1;
 		for (let i = 0; i < span.length; ) {
-			if (mode === CHILDREN_MODE) {
-				CHILDREN_RE.lastIndex = i;
-				const match = CHILDREN_RE.exec(span);
-				if (match) {
-					let before = span.slice(i, match.index);
-					i = match.index + match[0].length;
-					if (lineStart) {
-						before = before.replace(/^\s*/, "");
-						lineStart = false;
-					}
-
-					const [, newline, comment, tagging, closer, tagName] = match;
-					if (newline) {
-						before =
-							match.index > 0 && span[match.index - 1] === "\\"
-								? // remove the backslash from the output
-								  before.slice(0, -1)
-								: before.replace(/\s*$/, "");
-						lineStart = true;
-					}
-
-					if (before) {
-						current.children.push({type: "value", value: before});
-					}
-
-					if (comment) {
-						if (i === span.length) {
-							mode = CLOSING_COMMENT_MODE;
-						}
-					} else if (tagging) {
-						// TODO: Consider runtime type checking
-						let tag: Tag = tagName;
-						if (expressing && i === span.length) {
-							tag = expressions[s];
-							expressing = false;
-						}
-
-						if (closer) {
-							// TODO: Use function names for components
-							if (!stack.length) {
-								throw new Error(`Unexpected closing tag named ${String(tag)}`);
-							} else if (closer !== "//" && current.tag !== tag) {
-								throw new Error(`Mismatched tag: ${String(tag)}`);
-							}
-
-							current = stack.pop()!;
-							mode = CLOSING_TAG_MODE;
-						} else {
-							stack.push(current);
-							const next = {
-								type: "element" as const,
-								tag,
-								props: null,
-								children: [],
-							};
-							current.children.push(next);
-							current = next;
-							mode = PROPS_MODE;
-						}
-					}
-				} else if (i < span.length) {
-					let after = span.slice(i);
-					if (s === spans.length - 1) {
-						after = after.replace(/\s*$/, "");
-					}
-
-					if (after) {
-						current.children.push({type: "value", value: after});
-					}
-
-					i = span.length;
-				}
-			} else if (mode === PROPS_MODE) {
-				PROPS_RE.lastIndex = i;
-				const match = PROPS_RE.exec(span);
-				if (match) {
-					i = match.index + match[0].length;
-					const [, closer, spread, name, string] = match;
-					if (closer) {
-						if (closer[0] === "/") {
-							// TODO: Do we have to throw an error here if the stack is empty
-							// self-closing tag
-							current = stack.pop()!;
-						}
-
-						mode = CHILDREN_MODE;
-					} else if (spread) {
-						if (i !== span.length || s >= spans.length - 1) {
-							throw new Error("Expression expected");
-						}
-
-						current.props = {...current.props, ...(expressions[s] as any)};
-						expressing = false;
-					} else if (name) {
-						if (string == null) {
-							if (i < span.length) {
-								// TODO: Does this work when an expression appears
-								// after the boolean prop?
-								current.props = {...current.props, ...{[name]: true}};
-							} else if (i !== span.length || s >= spans.length - 1) {
-								throw new Error("Expression expected");
-							} else {
-								// prop expression
-								current.props = {...current.props, ...{[name]: expressions[s]}};
-								expressing = false;
-							}
-						} else {
-							current.props = {
-								...current.props,
-								// I accidentally made some regular expression emoticons ^-^
-								...{[name]: string.replace(/^('|")/, "").replace(/('|")$/, "")},
-							};
-						}
-					}
-				} else {
-					// TODO: Better diagnostic
-					throw new Error("Unexpected character");
-				}
-			} else if (mode === CLOSING_TAG_MODE) {
-				CLOSING_TAG_RE.lastIndex = i;
-				const match = CLOSING_TAG_RE.exec(span);
-				if (match) {
-					i = match.index + match[0].length;
-					mode = CHILDREN_MODE;
-				} else {
-					// TODO: Better diagnostic
-					throw new Error("Unexpected character");
-				}
-			} else if (mode === CLOSING_COMMENT_MODE) {
-				const ci = span.indexOf("-->");
-				if (ci === -1) {
+			if (typeof matcher === "string") {
+				// The only matcher which is a string right now is the
+				// CLOSING_COMMENT_MATCHER.
+				i = span.indexOf(matcher);
+				if (i === -1) {
 					break;
 				}
 
-				i = ci + "-->".length;
-				mode = CHILDREN_MODE;
+				i += matcher.length;
+				matcher = CHILDREN_MATCHER;
+			} else {
+				matcher.lastIndex = i;
+				const match = matcher.exec(span);
+				if (match) {
+					let before = span.slice(i, match.index);
+					i = match.index + match[0].length;
+					if (matcher === CHILDREN_MATCHER) {
+						if (lineStart) {
+							before = before.replace(/^\s*/, "");
+							lineStart = false;
+						}
+
+						const [, newline, comment, tagging, closer, tagName] = match;
+						if (newline) {
+							before =
+								match.index > 0 && span[match.index - 1] === "\\"
+									? // remove the backslash from the output
+									  before.slice(0, -1)
+									: before.replace(/\s*$/, "");
+							lineStart = true;
+						}
+
+						if (before) {
+							current.children.push({type: "value", value: before});
+						}
+
+						if (comment) {
+							if (i === span.length) {
+								matcher = CLOSING_COMMENT_MATCHER;
+							}
+						} else if (tagging) {
+							// TODO: Consider runtime type checking
+							let tag: Tag = tagName;
+							if (expressing && i === span.length) {
+								tag = expressions[s] as Tag;
+								expressing = false;
+							}
+
+							if (closer) {
+								// TODO: Use function names for components
+								if (!stack.length) {
+									throw new Error(
+										`Unexpected closing tag named ${String(tag)}`,
+									);
+								} else if (closer !== "//" && current.tag !== tag) {
+									throw new Error(`Mismatched tag: ${String(tag)}`);
+								}
+
+								current = stack.pop()!;
+								matcher = CLOSING_TAG_MATCHER;
+							} else {
+								stack.push(current);
+								const next = {
+									type: "element" as const,
+									tag,
+									props: null,
+									children: [],
+								};
+								current.children.push(next);
+								current = next;
+								matcher = PROPS_MATCHER;
+							}
+						}
+					} else if (matcher === PROPS_MATCHER) {
+						const [, closer, spread, name, string] = match;
+						if (closer) {
+							if (closer[0] === "/") {
+								// TODO: Do we have to throw an error here if the stack is empty
+								// self-closing tag
+								current = stack.pop()!;
+							}
+
+							matcher = CHILDREN_MATCHER;
+						} else if (spread) {
+							if (i !== span.length || s >= spans.length - 1) {
+								throw new Error("Expression expected");
+							}
+
+							current.props = {...current.props, ...(expressions[s] as any)};
+							expressing = false;
+						} else if (name) {
+							if (string == null) {
+								if (i < span.length) {
+									// TODO: Does this work when an expression appears
+									// after the boolean prop?
+									current.props = {...current.props, ...{[name]: true}};
+								} else if (i !== span.length || s >= spans.length - 1) {
+									throw new Error("Expression expected");
+								} else {
+									// prop expression
+									current.props = {
+										...current.props,
+										...{[name]: expressions[s]},
+									};
+									expressing = false;
+								}
+							} else {
+								current.props = {
+									...current.props,
+									// I accidentally made some regular expression emoticons ^-^
+									...{
+										[name]: string.replace(/^('|")/, "").replace(/('|")$/, ""),
+									},
+								};
+							}
+						}
+					} else if (matcher === CLOSING_TAG_MATCHER) {
+						matcher = CHILDREN_MATCHER;
+					}
+				} else {
+					if (matcher === CHILDREN_MATCHER) {
+						if (i < span.length) {
+							let after = span.slice(i);
+							if (s === spans.length - 1) {
+								after = after.replace(/\s*$/, "");
+							}
+
+							if (after) {
+								current.children.push({type: "value", value: after});
+							}
+
+							break;
+						}
+					} else {
+						// TODO: Better diagnostic
+						throw new Error("Unexpected character");
+					}
+				}
 			}
 		}
 
+		// TODO: I feel like there’s a more elegant way to express this. Too
+		// much custom logic right now.
 		if (expressing) {
 			const value = expressions[s];
-			if (mode === CHILDREN_MODE) {
+			if (matcher === CHILDREN_MATCHER) {
 				current.children.push({type: "value", value});
-			} else if (mode !== CLOSING_COMMENT_MODE) {
+			} else if (matcher !== CLOSING_COMMENT_MATCHER) {
 				throw new Error(
 					`Unexpected expression: \${${JSON.stringify(value, null, 2)}}`,
 				);
