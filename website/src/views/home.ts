b8808239a8d5dccf3fb5173e20d59f1302f8e50a
Brian Kim <briankimpossible@gmail.com>
2023-02-17 13:00:19 -0800

    tweaks (and lint)

--- website/src/views/home.ts
+++ website/src/views/home.ts
@@ -19,22 +19,25 @@ const components = {
 					text-align: center;
 					color: var(${depth === 2 ? "--text-color" : "--highlight-color"});
 					margin: ${depth === 2 ? "100px" : depth === 3 ? "50px" : 0} auto;
-					font-size: ${depth === 2 ? "5vh" : depth === 3 ? "4vh" : null}
+					font-size: ${depth === 2 ? "5vh" : depth === 3 ? "4vh" : null};
 				`}"
 			>
 				${children}
 			<//Tag>`;
 	},
 
-	paragraph({token, children}: any) {
+	paragraph({children}: any) {
 		return jsx`
 			<p class=${css`
+				padding: 1em;
 				@media screen and (min-width: 800px) {
 					margin: 1em auto 0;
 					max-width: 1000px;
 				}
 			`}>
-				<span class=${css`box-decoration-break: clone;`}>${children}</span>
+				<span class=${css`
+					box-decoration-break: clone;
+				`}>${children}</span>
 			</p>
 		`;
 	},
@@ -100,52 +103,49 @@ export default async function Home({storage}: {storage: Storage}) {
 				<h1 class=${css`
 					margin: 30px 0;
 					color: var(--highlight-color);
-					font-size: 50px;
-					@media screen and (min-width: 800px) {
-						font-size: 10vw;
-					}
+					font-size: max(50px, 10vw);
 				`}
 				>Crank.js</h1>
 				<h2
 					class=${css`
 						margin: 0;
 						color: var(--text-color);
-						font-size: 24px;
-						@media screen and (min-width: 800px) {
-							font-size: 4vw;
-						}
+						font-size: max(30px, 4vw);
 					`}
 				>
-					The Just JavaScript Framework
+					The Just JavaScript Framework.
 				</h2>
 			</header>
 			<div class=${css`
-				padding: 1em 0;
-				font-size: 22px;
+				font-size: 16px;
+				@media screen and (min-width: 800px) {
+					font-size: 24px;
+				}
+
 				background-color: var(--bg-color);
 				border-top: 1px solid currentcolor;
 			`}>
 				<${Marked} markdown=${md.body} components=${components} />
-			</div>
-			<div class=${css`
-				text-align: center;
-				height: 100vh;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-			`}>
-				<a
-					href="/guides/getting-started"
-					class=${css`
-						display: inline-block;
-						border: 1px solid #dbb368;
-						color: #dbb368;
-						padding: 20px;
-						margin: 50px 0;
-						text-decoration: none;
-						font-size: 24px;
-					`}
-				>Get Started</a>
+				<div class=${css`
+					text-align: center;
+					height: 100vh;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+				`}>
+					<a
+						href="/guides/getting-started"
+						class=${css`
+							display: inline-block;
+							border: 1px solid #dbb368;
+							color: #dbb368;
+							padding: 20px;
+							margin: 50px 0;
+							text-decoration: none;
+							font-size: 24px;
+						`}
+					>Get Started</a>
+				</div>
 			</div>
 		<//Root>
 	`;
