a8a8476bf7f73f491d43adc18fd5d568e0d0d275
Brian Kim <briankimpossible@gmail.com>
2023-02-25 00:35:45 -0800

    delete some shit

--- website/src/build.ts
+++ website/src/build.ts
@@ -6,17 +6,6 @@ import {renderStylesToString} from "@emotion/server";
 import FS from "fs-extra";
 import * as Path from "path";
 
-// TODO: lazily import these?
-// TODO: I do not understand the importance of these imports. I may very well delete them.
-// Watch out. I am a madman. I MIGHT ACTUALLY DELETE THESE.
-import "prismjs";
-import "prismjs/components/prism-javascript.js";
-import "prismjs/components/prism-jsx.js";
-import "prismjs/components/prism-typescript.js";
-import "prismjs/components/prism-tsx.js";
-import "prismjs/components/prism-diff.js";
-import "prismjs/components/prism-bash.js";
-
 import {collectDocuments} from "./models/document.js";
 import {Storage} from "./components/esbuild.js";
 import {router} from "./routes.js";
@@ -82,80 +71,6 @@ async function build(urls: Array<string>) {
 		await FS.ensureDir(Path.join(dist, url));
 		await FS.writeFile(Path.join(dist, url, "index.html"), html);
 	}
-	//{
-	//	// GUIDES
-	//	const docs = await collectDocuments(
-	//		path.join(__dirname, "../documents/guides"),
-	//		path.join(__dirname, "../documents/"),
-	//	);
-	//
-	//	await Promise.all(
-	//		docs.map(async (post) => {
-	//			const {
-	//				attributes: {publish},
-	//				url,
-	//			} = post;
-	//			if (!publish) {
-	//				return;
-	//			}
-	//
-	//			const match = router.match(url);
-	//			if (!match) {
-	//				return;
-	//			}
-	//
-	//			const html = await renderer.render(jsx`
-	//				<${Guide} url=${url} storage=${storage} params=${match.params} />
-	//			`);
-	//
-	//			const filename = path.join(dist, url + ".html");
-	//			await fs.ensureDir(path.dirname(filename));
-	//			await fs.writeFile(filename, html);
-	//		}),
-	//	);
-	//import BlogHome from "./views/blog-home.js";
-	//{
-	//	const html = await renderer.render(jsx`
-	//		<${BlogHome} storage=${storage} />
-	//	`);
-	//
-	//	await fs.ensureDir(path.join(dist, "blog"));
-	//	await fs.writeFile(path.join(dist, "blog/index.html"), html);
-	//}
-	//
-	//import BlogPage from "./views/blog.js";
-	//{
-	//	// BLOG POSTS
-	//	const posts = await collectDocuments(
-	//		path.join(__dirname, "../documents/blog"),
-	//		path.join(__dirname, "../documents/"),
-	//	);
-	//	posts.reverse();
-	//	await Promise.all(
-	//		posts.map(async (post) => {
-	//			const {
-	//				attributes: {publish},
-	//				url,
-	//			} = post;
-	//			if (!publish) {
-	//				return;
-	//			}
-	//
-	//			const match = router.match(url);
-	//			if (!match) {
-	//				return;
-	//			}
-	//
-	//			const html = await renderer.render(jsx`
-	//				<${BlogPage} url=${url} storage=${storage} />
-	//			`);
-	//
-	//			const filename = path.join(dist, url + ".html");
-	//			await fs.ensureDir(path.dirname(filename));
-	//			await fs.writeFile(filename, html);
-	//		}),
-	//	);
-	//}
 }
 
 await build(urls);
