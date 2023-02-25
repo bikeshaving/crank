232022e9fc746e16945515617db7288518b76793
Brian Kim <briankimpossible@gmail.com>
2023-02-24 21:15:11 -0800

    initial attempt at getting build command working again

---
+++ website/src/router.ts
@@ -0,0 +1,53 @@
+import {compile, match} from "path-to-regexp";
+import type {MatchFunction, PathFunction} from "path-to-regexp";
+
+export interface Route {
+	name: string;
+	match: MatchFunction;
+	reverse: PathFunction;
+}
+
+export function route(
+	matcher: string,
+	name: string,
+	// TODO: options?
+): Route {
+	return {
+		name,
+		match: match(matcher),
+		reverse: compile(matcher),
+	};
+}
+
+export interface Router {
+	routes: Array<Route>;
+}
+
+export class Router {
+	constructor(routes: Array<Route>) {
+		this.routes = routes;
+	}
+
+	match(pathname: string): MatchResult | null {
+		for (const route of this.routes) {
+			let match = route.match(pathname);
+			if (match) {
+				return {
+					name: route.name,
+					params: match.params as Record<string, unknown>,
+				};
+			}
+		}
+
+		return null;
+	}
+
+	reverse(_result: MatchResult): string {
+		throw new Error("TODO");
+	}
+}
+
+export interface MatchResult {
+	name: string;
+	params: Record<string, unknown>;
+}
